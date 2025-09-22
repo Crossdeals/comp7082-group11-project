6a3c844c9e00b5c8c0bf49c988353e7ba8850fbc
Jeffrey Chou <jeffreychou1991@gmail.com>
2025-09-21 22:13:15 -0700

    database setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 
 require('dotenv').config();
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Cross Deals!');
