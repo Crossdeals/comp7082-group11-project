2d83c17880e736af3933fc72192321ddceb5d385
Jeff Chou <me@example.com>
2025-09-28 21:14:45 -0700

    backend user creation, login with passsport

--- src/app.js
+++ src/app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const accountRoute = require('./routes/accountRoutes');
+const accountController = require('./controllers/accountController');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
 
+app.use(
+    session({
+        secret: "Crossdeals",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.authenticate('session'));
 require('dotenv').config();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 connectDB();
 
 app.use("/", accountRoute);
+app.use("/", accountController);
 
 module.exports = app;
--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -9,26 +9,4 @@ router.get("/", (req, res) => {
     res.send("Crossdeals account creation/login screen");
 });
 
-router.get("/test", async (req,res)=>{
-    try {
-        const results = await User.find();
-        console.log(results);
-    } catch(err) {}
-
-    res.send("done");
-});
-
-router.post("/", jsonParser, async (req, res) => {
-    console.log("got post request ", req.body);
-    res.send("got post request");
-    const data  = req.body;
-    console.log(data);
-    const newUser = new User(req.body);
-    try {
-        var test = await newUser.save();
-    } catch(err){
-        console.log("error saving ", err);
-    }
-});
-
 module.exports = router;
