691f7ad3240a84c4218b38602c85b966175011df
Jeffrey Chou <jeffreychou1991@gmail.com>
2025-10-07 01:16:03 -0700

    hook up frontend to backend

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -2,11 +2,25 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
 const User = require('../models/UserModel');
+const path = require('path');
 
 const jsonParser = bodyParser.json();
+const frontendPath = path.join(__dirname, '..', '..', 'crossdeals-frontend','html');
 
 router.get("/", (req, res) => {
-    res.send("Crossdeals account creation/login screen");
+    res.sendFile(path.join(frontendPath,'login.html'));
+});
+
+router.get("/login.html", (req, res) => {
+    res.sendFile(path.join(frontendPath,'login.html'));
+});
+
+router.get("/signup.html", (req, res) => {
+    res.sendFile(path.join(frontendPath,'signup.html'));
+});
+
+router.get("/soon", (req,res)=>{
+    res.sendFile(path.join(frontendPath, '..', 'index.html'));
 });
 
 module.exports = router;
