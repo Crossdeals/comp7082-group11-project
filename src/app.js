6a70e152e167c8c9cb6409d4d6634452bdfb85c4
fortyfiveacp <nng32@my.bcit.ca>
2025-10-13 15:22:32 -0700

    Setup CORS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,16 @@ app.use(
 );
 app.use(cookieParser());
 
+const allowHeaders = (req, res, next) => {
+    // If hosted, set the origin to the FE address.
+    res.header('Access-Control-Allow-Origin', `null`);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+}
+
+app.use(allowHeaders);
 
 app.use(express.json());
 app.use(bodyParser.json());
