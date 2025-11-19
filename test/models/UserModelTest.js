151a3be3ca13544c3f3e3d5edb7ebf547aa11d5c
Jeffrey Chou <jeffreychou1991@gmail.com>
2025-11-19 15:31:09 -0800

    remaining user model tests

--- test/models/UserModelTest.js
+++ test/models/UserModelTest.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const User = require("../../src/models/UserModel")
+const mongoose = require("mongoose");
 
 describe("User Model Unit Tests", () => {
 
@@ -25,4 +26,47 @@ describe("User Model Unit Tests", () => {
         });
     });
 
+    it("should fail to create User if userName is empty", async () =>{ 
+        let error;
+        try {
+            await createTestUser("");
+        }
+        catch(e) {
+            error = e;
+        }
+        expect(error).instanceOf(mongoose.Error.ValidationError);
+        const count = await User.collection.countDocuments();
+        expect(count).is.equal(0);
+    });
+
+    it("should fail to create User if password is empty", async () =>{ 
+        let error;
+        try {
+            await createTestUser(testUserName, "");
+        }
+        catch(e) {
+            error = e;
+        }
+        expect(error).instanceOf(mongoose.Error.ValidationError);
+        const count = await User.collection.countDocuments();
+        expect(count).is.equal(0);
+    });
+
+    it("should fail to create User if username not unique", async () =>{ 
+        const usersToInsert = [
+            { userName: testUserName, password: testPassword },
+            { userName: testUserName, password: testPassword }
+        ]
+        let error;
+        try {
+            await User.insertMany(usersToInsert);
+        }
+        catch(e) {
+            error = e;
+        }
+        expect(error.code).to.equal(11000);
+        expect(error.message).to.include("duplicate key error");
+        const count = await User.collection.countDocuments();
+        expect(count).is.equal(1);
+    });
 });
