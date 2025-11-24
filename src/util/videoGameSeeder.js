ef09a236945956b88bb5be3e1f1ead46b664b65e
Jeffrey Chou <jeffreychou1991@gmail.com>
2025-11-23 18:38:22 -0800

    updated videogame seeder to have more titles and each title hass a variable amount of deal data

--- src/util/videoGameSeeder.js
+++ src/util/videoGameSeeder.js
@@ -1,61 +1,72 @@
 const Storefront = require('../models/StorefrontModel');
 const VideoGame = require('../models/VideoGameModel');
 
-const seedData = [
-    {
-        title: "Minecraft",
-        deals: [
-            {
-                storefront: null,
-                originalPrice: 99.99,
-                currentPrice: 59.99,
-                bestPrice: 39.99,
-                dealEndDate: "2025-11-11"
-            }
-        ]
-    },
-    {
-        title: "Starcraft",
-        deals: [
-            {
-                storefront: null,
-                originalPrice: 29.99,
-                currentPrice: 29.99
-            }
-        ]
-    }
-];
+const gameTitles = [
+    "Minecraft",
+    "Starcraft",
+    "Warcraft",
+    "Risk of Rain 2",
+    "Heavy Rain",
+    "Monster Train 2",
+    "Slay the Spire",
+    "Monster Hunter World",
+    "Monster Hunter Rise"
+]
+
+const dealDataTemplate = {
+    storefront: null,
+    originalPrice: 0,
+    currentPrice: 0,
+    bestPrice: 0,
+    dealEndDate: null
+}
 
 const seedVideoGames = async () => {
+    
+    
     try {
+        await VideoGame.deleteMany({});
         const store = await Storefront.findOne({});
         if(!store) {
             throw new Error("No storefront found, please seed the Storefront document");
         }
-        for (let i = 0; i < seedData.length; i++) {
-            let game = seedData[i];
-            let deal = game.deals[0];
-            deal.storefront = store._id;
-            foundGame = await VideoGame.findOne({title: game.title});
-            if(foundGame) {
-                console.log("skipping game");
-                continue;
-            }
-            console.log("creating new game");
-            const newGame = new VideoGame({title: game.title});
-            newGame.deals.push({
-                storefront: deal.storefront,
-                originalPrice: deal.originalPrice,
-                currentPrice: deal.currentPrice,
-                bestPrice: deal?.bestPrice,
-                dealEndDate: deal?.dealEndDate
-            });
-            await newGame.save();
-    }
+        await seedGames();
+    
     } catch(error) {
         console.error(`Error: ${error.message}`);
     }
 }
 
 module.exports = seedVideoGames;
+
+const seedGames = async function() {
+    const storeCount = await Storefront.countDocuments();
+    
+    for(let i = 0; i < gameTitles.length; i++) {
+        const randomInt = Math.floor( Math.random() * storeCount + 1);
+        const randomStores = await Storefront.aggregate([{ $sample: { size: randomInt}}])
+        const dealInfoList = generateRandomDealData(randomStores);
+        const newGame = new VideoGame({title: gameTitles[i]});
+        newGame.deals = dealInfoList;
+        newGame.markModified("deals");
+        await newGame.save();
+    }
+}
+
+const generateRandomDealData = function(randomStores) {
+    const dealInfo = [];
+    for(let i = 0; i < randomStores.length; i++) {
+        const dealData = { ...dealDataTemplate};
+        dealData.storefront = randomStores[i]._id;
+        const originalPrice = Math.random() * 80 + 20;
+        const currentPrice = Math.random() * originalPrice + 1;
+        dealData.originalPrice = originalPrice.toFixed(2);
+        dealData.currentPrice = currentPrice.toFixed(2);
+        dealData.bestPrice = dealData.currentPrice;
+        const randomDay = Math.floor(Math.random() * 31 + 1);
+        dealData.dealEndDate = "2025-12-" + (randomDay < 10 ? "0": "" ) + randomDay;
+        dealInfo.push(dealData);
+    }
+    return dealInfo;
+}
 
