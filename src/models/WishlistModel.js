20510a83c4132d3d77256eeb3be589f061436f7f
Jeffrey Chou <jeffreychou1991@gmail.com>
2025-11-24 17:38:16 -0800

    Fixed wishlist controller storing the wrong data (#26)
    * don't try to add full game when you only want the id
    
    * the root cause of the issue was an accidental schema change in wishlist
    
    * added test to catch if wishlist is not saving video games as mongoose object ids
    
    * reverting fix to verify test automation will catch this issue
    
    * putting fix back in

--- src/models/WishlistModel.js
+++ src/models/WishlistModel.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 
 const WishlistModelSchema = new Schema({
     preferredStores: { type: [String], ref: 'Storefront'},
-    games: { type: [String], ref: 'VideoGame' }
+    games: { type: [Schema.Types.ObjectId], ref: 'VideoGame' }
 });
 
 const Wishlist = mongoose.model('Wishlist', WishlistModelSchema);
